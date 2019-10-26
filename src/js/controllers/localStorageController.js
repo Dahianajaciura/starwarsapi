f7d6208fa5466dd39ba88b4a9285776d9d41cd58
Dahiana <djaciura@gmail.com>
2019-10-26 16:25:45 -0300

    arreglo del localStorageController

--- src/js/controllers/localStorageController.js
+++ src/js/controllers/localStorageController.js
@@ -1,5 +1,8 @@
+import showSaved from "../utils/showSaved";
+
 function localStorageController() {
   console.log("Se cargo el localStorage");
+  showSaved()
 }
 
 export default localStorageController;
